ce673d1c1126268cf8dd6dfb487ed8512471559e
Humberto Asca <hasca@ciplima.org.pe>
2022-10-01 14:14:57 -0500

    Add: Routing for req.params with one route. Example /api/:dateRequired

--- app.js
+++ app.js
@@ -5,9 +5,50 @@ const port = 3000
 // var msg = 'Hello xxxx World';
 // console.log(msg);
 
-app.get('/', (req, res) => {
-  res.send('hello xxxxworld')
-})
+// app.get('/', (req, res) => {
+//   res.send('hello World .....')
+// })
+
+/** Serve an HTML file */
+app.get("/", function (req, res) {
+  // res.send("Hello Express is running dude! xxxxx ");
+  // res.sendFile(absolutePath);
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/api', (req, res) => {
+  const d = new Date();
+  req.time = new Date().toString();
+  // let text = d.toUTCString();
+  res.json({ 
+      UTC : `${d.toUTCString()}`,
+      Peru_time : `${req.time}`})
+});
+
+app.get('/api/:dateRequired', (req, res) => {
+  let timeWithDash = "", timeInMiliseconds = ""
+  
+  //Getting the req.params
+  const {dateRequired} = req.params
+  // console.log(dateRequired);
+
+  //Date with dashes
+  if (dateRequired.indexOf("-") != -1) {
+    const result = dateRequired.split("-");
+    console.log("Resultado: ", result);
+    
+    //Getting the UTC (GMT)  format
+    const d = new Date(`${result[0]}-${result[1]}-${result[2]}`);
+
+    let ms =  Date.parse(`${result[0]}-${result[1]}-${result[2]}`);
+
+    res.json({"unix": ms, "UTC": d.toUTCString() });
+
+  } else { //Date in Milisecons format
+    const d = new Date(parseInt(dateRequired));
+    res.json({"unix": parseInt(dateRequired), "UTC": d.toUTCString() });
+  }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
